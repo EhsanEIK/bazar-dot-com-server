552518984e3cd292d9521af0533e34d4ac040de2
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-23 12:21:54 +0600

    added orders GET single api to get the single data from db

--- index.js
+++ index.js
@@ -188,6 +188,14 @@ async function run() {
             res.send(orders);
         })
 
+        // orders [GET-Signle Data]
+        app.get('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const order = await ordersCollection.findOne(query);
+            res.send(order);
+        })
+
         // orders [POST]
         app.post('/orders', async (req, res) => {
             const order = req.body;
